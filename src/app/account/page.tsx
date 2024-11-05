cc502c570c327b92f6a576dff6f5e23d5575a382
Parth Bhalerao <iamparthhh@gmail.com>
2024-11-04 22:25:41 -0500

    Fixed account page

---
+++ src/app/account/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+
+export default function AccountPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Account Page</h1>
+      {/* Add your account page content here */}
+    </div>
+  );
+}
